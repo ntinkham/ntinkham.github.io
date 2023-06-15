099faf93857598502309ac620cfd6d317ad86110
ntinkham <136725415+ntinkham@users.noreply.github.com>
2023-06-15 10:49:05 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+function seeMore(){
+    let seeMoreP = document.getElementById("see-more");
+    if (seeMoreP.classList.contains("hidden")) {
+        seeMoreP.classList.remove("hidden");
+        button.innerHTML = "See Less" ;
+    }
+    
+    else {
+        seeMoreP.classList.add("hidden");
+        button.innerHTML = "See More" ;
+    }
+}
+
+function secondSeeMore(){
+    let seeMoreP = document.getElementById("see-again");
+    if (seeMoreP.classList.contains("hidden")) {
+        seeMoreP.classList.remove("hidden");
+        click.innerHTML = "See Less" ;
+    }
+    
+    else {
+        seeMoreP.classList.add("hidden");
+        click.innerHTML = "See More" ;
+    }
+}
+
+function displayResults(){
+    let questionOne=document.getElementById("one").value;
+    let questionTwo=document.getElementById("two").value;
+    let questionThree=document.getElementById("three").value;
+    let questionFour=document.getElementById("four").value;
+    
+    let button=document.getElementById("submit").click;
+    
+    let resultsDiv=document.getElementById("results");
+    resultsDiv.classList.remove("hidden");
+    score = 0;
+    
+    if (questionOne == "image") {
+        resultsDiv.innerHTML += "<p>" + "Question  1 - " + "Correct";
+        score = score + 1;
+    }
+    
+    else {
+        resultsDiv.innerHTML += "<p>" + "Question  1 - " + "Incorrect";
+    }
+    
+    if (questionTwo == "para") {
+        resultsDiv.innerHTML += "<p>" + "Question  2 - " + "Correct";
+        score = score + 1;
+    }
+    
+    else {
+        resultsDiv.innerHTML += "<p>" + "Question  2 - " + "Incorrect";
+    }
+    
+    if (questionThree == "cascading") {
+        resultsDiv.innerHTML += "<p>" + "Question  3 - " + "Correct";
+        score = score + 1;
+    }
+    
+    else {
+        resultsDiv.innerHTML += "<p>" + "Question  3 - " + "Incorrect";
+    }
+    
+    if (questionFour == "align") {
+        resultsDiv.innerHTML += "<p>" + "Question  4 - " + "Correct";
+        score = score + 1;
+    }
+    
+    else {
+        resultsDiv.innerHTML += "<p>" + "Question  4 - " + "Incorrect";
+    }
+    
+    resultsDiv.innerHTML += "Score: " + score + "/4;"
+}
